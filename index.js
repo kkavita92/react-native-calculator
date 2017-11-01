6fc99632c5fa66809b10dcfcf7456dfda6665954
Kavita Kalaichelvan <kkavita92@gmail.com>
2017-11-01 15:41:13 +0000

    Setup background colours

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import {
+    View,
     Text,
-    AppRegistry
+    AppRegistry //Used to ensure JS execution environment is setup
 } from 'react-native';
-
-AppRegistry.registerComponent('ReactCalculator', () => ReactCalculator);
 
 class ReactCalculator extends Component {
 
   render() {
-    return (
-      <Text>Hello, React!</Text>
+    return ( //Creates container View that wraps two sections and takes full width and height
+      <View style={{flex: 1}}>
+        <View style={{flex: 2, backgroundColor: '#193441'}}></View>
+        <View style={{flex: 8, backgroundColor: '#3E606F'}}></View>
+      </View>
     )
   }
 
 }
+
+AppRegistry.registerComponent('ReactCalculator', () => ReactCalculator); //For app root components to register themselves for system to load the bundle for the app
